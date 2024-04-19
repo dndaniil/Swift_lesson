1a782a7b8a53de1984b35074bd45ab9971f13081
dndaniil <gedase2005@gmail.com>
2024-04-19 21:13:53 +0300

    help with variables and constants

--- Base_swift/main.swift
+++ Base_swift/main.swift
@@ -1,1 +1,34 @@
+//var - переменная
+//let - константа
 
+
+//let userName: String = "test"
+//let b = 3
+//let c: Float = 3.5
+//let d = "fjfjfj"
+//let f: Character = "f"
+//let userNames = "test"
+//
+//print(userName, b, c, d, f, userNames)
+
+
+
+//есть два способа и лет(константа) не меняет свое значение
+//let carName: String
+//carName = "tesla"
+//print(carName)
+//
+//let carName = "Tesla"
+//print(carName)
+
+
+//var  может менять сво значение
+//
+//var userName = 2
+//print(userName)
+//
+//var userName: Int
+//userName = 10
+//print(userName)
+//userName = 15
+//print(userName)
